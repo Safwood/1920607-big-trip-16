9049cbbce34ea50a886646a48d6084f5a96c6e68
safwood <naste-f@yandex.ru>
2021-12-20 12:51:49 +0300

    check eslint

--- src/utils/convertDate.js
+++ src/utils/convertDate.js
@@ -1,20 +1,8 @@
 import dayjs from 'dayjs';
 
-export const convertDateWithYear = (date) => {
-  return dayjs(date).format('YYYY/MM/DD')
-}
-export const convertTimeForSorting = (date) => {
-  return dayjs(date).format('HH/mm');
-}
-export const convertDateWithTime = (date) => {
-  return dayjs(date).format('YYYY-MM-DDTHH:mm');
-}
-export const convertDateWithMonth = (date) => {
-  return dayjs(date).format('MMM DD');
-}
-export const convertDateWithDay = (date) => {
-  return dayjs(date).format('DD/MM/YY');
-}
-export const getTime = (date) => {
-  return dayjs(date).format('HH/mm');
-}
+export const convertDateWithYear = (date) => dayjs(date).format('YYYY/MM/DD');
+export const convertTimeForSorting = (date) => dayjs(date).format('HH/mm');
+export const convertDateWithTime = (date) => dayjs(date).format('YYYY-MM-DDTHH:mm');
+export const convertDateWithMonth = (date) => dayjs(date).format('MMM DD');
+export const convertDateWithDay = (date) => dayjs(date).format('DD/MM/YY');
+export const getTime = (date) => dayjs(date).format('HH/mm');
